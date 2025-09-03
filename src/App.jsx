6d5d9e10ea4460124d576415b16ad24ef0ede7f6
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2025-09-03 23:16:12 +0600

    installed flowbite and created basic navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-5xl bg-red-200 p-5'>Vite + React</h1>
+      <Navbar></Navbar>
      
     </>
   )
