173003c88d6d68efcac4f55c4ea5a7f0dbaeca68
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2025-09-06 23:49:01 +0600

    showing all jobs on see all jobs button click

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -3,25 +3,33 @@ import Job from "./Job";
 
 const Jobs = () => {
     const [Jobs, setJobs] = useState([]);
+    const [allJobs, setAllJobs] = useState(false);
     useEffect(() => {
         fetch('jobs.json')
             .then(res => res.json())
             .then(data => setJobs(data))
     }, [])
+    
     return (
         <div className="max-w-screen-xl mx-auto">
             <div className="text-center space-y-4">
                 <h2 className="font-extrabold text-2xl md:text-5xl">Featured Jobs</h2>
                 <p className="font-medium text-[#757575]">Explore thousands of job opportunities with all the information you need. Its your future</p>
             </div>
-            <div className="mt-8 grid gap-6 grid-cols-1 lg:grid-cols-2">
-                {
-                    Jobs.slice(0,4).map(job => <Job key={job.id} job={job}></Job>)
-                }
 
+            {allJobs ? <div className="mt-8 grid gap-6 grid-cols-1 lg:grid-cols-2">
+                {
+                    Jobs.map(job => <Job key={job.id} job={job}></Job>)
+                }
             </div>
+                : <div className="mt-8 grid gap-6 grid-cols-1 lg:grid-cols-2">
+                    {
+                        Jobs.slice(0, 4).map(job => <Job key={job.id} job={job}></Job>)
+                    }
+                </div>}
+
             <div className="text-center mt-10">
-                 <button className="btn py-5 rounded-lg px-7 h-[50px] text-white bg-gradient-to-r from-blue-500 to-purple-500">See All Jobs</button>
+                <button onClick={() => setAllJobs(true)} className="btn py-5 rounded-lg px-7 h-[50px] text-white bg-gradient-to-r from-blue-500 to-purple-500">See All Jobs</button>
             </div>
         </div>
     );
